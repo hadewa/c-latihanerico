1dead0bde0989ba5005d4aa11e73770b40a1b6b7
hadewa <rivaldihandaka@gmail.com>
2018-07-10 17:23:09 +0700

    Add files via upload

---
+++ Bab 2/Evaluasi/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int a, c;
+	
+	a = 10;
+	c = ++a - 1;
+	
+	printf("%d", c);
+}
---
+++ Bab 2/Evaluasi/4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	float a;
+	
+	a = 18 + 7 * 5 / (2*2) + 1 - 17;
+	printf("%.2f", a);
+}
